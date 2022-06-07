6b3d89a51f239c34278cc0cbecfa9ad0ea32fb07
eradoria <eradoria@gmail.com>
2022-06-07 18:33:35 -0500

    final

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 // import axios here
+const axios = require('axios')
 
 // Please note that it is normally not considered best practice to commit 
 // api keys to github as it presents a security risk. It is done here only 
@@ -7,21 +8,47 @@ const api_key = 'd771b19ef336ed8381def3a60b574464'
 
 const discoverMovie = () => {
   const url = `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}`
-  // code here
+  // code here 
+  let results = axios(url).then(res => {
+    console.log(res.data)
+    return res
+  })
+  // console.log(results)
+  return results
 }
+
+discoverMovie()
 
 const getMovieById = (id) => {
   const url = `https://api.themoviedb.org/3/movie/${id}?api_key=${api_key}`
   // code here
+  let results = axios(url).then(res => {
+    console.log(res.data)
+    return res.data
+  })
+  // console.log(results)
+  return results
 }
+
+getMovieById(3)
 
 const getMovieByIdFailure = () => {
-  const fakeId = 1 // FAKE ID HERE
+  const fakeId = 5783 // FAKE ID HERE
   const url = `https://api.themoviedb.org/3/movie/${fakeId}?api_key=${api_key}`
   // code here
+  let results = axios(url).then(res => {
+        // console.log(res)
+        return res
+      })
+      .catch(error => {
+        console.log(error)
+        return error.response.status
+      })
+      // console.log(results)
+      return results
 }
 
-
+getMovieByIdFailure()
 
 module.exports = {
   discoverMovie,
